4d98c68172c49c55bd37a725aae339919d022992
ForberT <50460628+ForberT@users.noreply.github.com>
2019-07-21 16:46:04 -0400

    Add files via upload
    Contents of the github folder created by the build command in the deployment instructions

---
+++ assets/data/mySong.ts
@@ -0,0 +1,32 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+    {
+        name:"Awakening",
+        artist:"Unleash The Archers",
+        genre:"Melodic Power Metal",
+        yearReleased:2017,
+        picture:"assets/images/apex.jpg"
+    },
+    {
+        name:"Hotel California",
+        artist:"Eagles",
+        genre:"Soft Rock",
+        yearReleased:1977,
+        picture:"assets/images/hotel-california.jpg"
+    },
+    {
+        name:"The Sound Of Silence",
+        artist:"Disturbed",
+        genre:"Piano Rock",
+        yearReleased:2015,
+        picture:"assets/images/sound-of-silence.jpg"
+    },
+    {
+        name:"Nothing But A Song",
+        artist:"Great Big Sea",
+        genre:"Folk Rock",
+        yearReleased:2010,
+        picture:"assets/images/safe-upon-the-shore.jpg"
+    }
+];
